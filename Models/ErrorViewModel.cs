1cbd3e3256f3846cfc3d7e5cbb7aac4b3bfb8330
Yurii Demyan <yra.demyan@gmail.com>
2026-03-23 10:03:46 +0200

    Project Start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sprint_13_Dy_Sa_Bo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
