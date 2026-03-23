a853d76fac6e8ca6c245cb26aac92391e7f6ad4c
Loki22978964 <lokim3059@gmail.com>
2026-03-23 21:55:22 +0200

    Task 8: implement ShoppingCart form with html-helpers and POST request

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Sprint_13_Dy_Sa_Bo.Models;
 using Sprint_13_Dy_Sa_Bo.Services;
 
@@ -33,6 +34,51 @@ namespace Sprint_13_Dy_Sa_Bo.Controllers
             };
 
             return View(dictionary);
+        }
+
+        [HttpGet]
+        public IActionResult ShoppingCart()
+        {
+            var supermarkets = new List<string> { "WellMart", "Silpo", "ATB" };
+            ViewBag.Supermarkets = new SelectList(supermarkets);
+
+            var dates = new List<DateTime>
+            {
+                DateTime.Today,
+                DateTime.Today.AddDays(1),
+                DateTime.Today.AddDays(2)
+            };
+            ViewBag.Dates = dates;
+
+            Dictionary<string, int> products = new Dictionary<string, int>
+            {
+                { "Bread", 10 },
+                { "Milk", 11 },
+                { "Cheese", 140 },
+                { "Sausage", 110 },
+                { "Potato", 7},
+                { "Banana", 23 },
+                { "Tomato", 25 },
+                { "Candy", 75 }
+            };
+
+            ViewBag.Products = new MultiSelectList(products.Keys);
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ShoppingCart(string fullName, string address)
+        {
+            if (fullName == null || address == null)
+            {
+                ViewBag.Message = $"enter correct dataenter correct data!!!!!!!";
+                return View();
+            }
+
+            ViewBag.Message = $"Your products will be shipped at: {address}.Bon appetite, {fullName}!";
+
+            return View();
         }
     }
 }
